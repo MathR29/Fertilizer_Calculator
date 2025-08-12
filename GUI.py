4c074a1382be481f70d6ed66871fc26cd5179d4f
MathR29 <mrosini29@gmail.com>
2025-08-12 10:56:13 -0300

    Main Menu

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 import Fertilization as fert
 import tkinter as tk
-from tkinter import Button, Entry, Label, Text, ttk
+from Insert_soil_data import insert_data
+from tkinter import LEFT, Button, Entry, Label, Text, ttk
 from PIL import ImageTk, Image
 
 root = tk.Tk()
@@ -106,7 +107,7 @@ calculate_button = Button(frame_calculadora,
                           text="Calcular",
                           font=("Arial", 14),
                           command=calculator,
-                          height=2,
+                          height=5,
                           width=10)
 calculate_button.pack(anchor="e", padx=10, pady=(0,15))
 
@@ -146,7 +147,61 @@ output_Lime = Text(frame_calculadora,
                    width=12)
 output_Lime.pack(anchor="w", padx=450, pady=(0,8))
 
+back_button = Button(frame_calculadora,
+                     text = "Voltar",
+                     command = lambda: select_frame("Menu"))
+back_button.pack()
 
+# ID da Parcela
+Label(frame_inserir_dados, text="ID da Parcela").grid(row=0, column=0, padx=5, pady=2, sticky="e")
+entry_new_plotid = Entry(frame_inserir_dados)
+entry_new_plotid.grid(row=0, column=1, padx=5, pady=2)
+
+# Argila (%)
+Label(frame_inserir_dados, text="Argila (%)").grid(row=1, column=0, padx=5, pady=2, sticky="e")
+entry_new_clay = Entry(frame_inserir_dados)
+entry_new_clay.grid(row=1, column=1, padx=5, pady=2)
+
+# P (mg/dm³)
+Label(frame_inserir_dados, text="P (mg/dm³)").grid(row=2, column=0, padx=5, pady=2, sticky="e")
+entry_new_p = Entry(frame_inserir_dados)
+entry_new_p.grid(row=2, column=1, padx=5, pady=2)
+
+# K (cmolc/dm³)
+Label(frame_inserir_dados, text="K (cmolc/dm³)").grid(row=3, column=0, padx=5, pady=2, sticky="e")
+entry_new_k = Entry(frame_inserir_dados)
+entry_new_k.grid(row=3, column=1, padx=5, pady=2)
+
+# CTC (cmolc/dm³)
+Label(frame_inserir_dados, text="CTC (cmolc/dm³)").grid(row=4, column=0, padx=5, pady=2, sticky="e")
+entry_new_ctc = Entry(frame_inserir_dados)
+entry_new_ctc.grid(row=4, column=1, padx=5, pady=2)
+
+# V (%)
+Label(frame_inserir_dados, text="V (%)").grid(row=5, column=0, padx=5, pady=2, sticky="e")
+entry_new_v = Entry(frame_inserir_dados)
+entry_new_v.grid(row=5, column=1, padx=5, pady=2)
+
+# Ca (cmolc/dm³)
+Label(frame_inserir_dados, text="Ca (cmolc/dm³)").grid(row=6, column=0, padx=5, pady=2, sticky="e")
+entry_new_ca = Entry(frame_inserir_dados)
+entry_new_ca.grid(row=6, column=1, padx=5, pady=2)
+
+# Mg (cmolc/dm³)
+Label(frame_inserir_dados, text="Mg (cmolc/dm³)").grid(row=7, column=0, padx=5, pady=2, sticky="e")
+entry_new_mg = Entry(frame_inserir_dados)
+entry_new_mg.grid(row=7, column=1, padx=5, pady=2)
+insert_button = Button(frame_inserir_dados,
+                       text = "Inserir Dados",
+                       command= lambda: insert_data(plot_id = entry_new_plotid.get(), 
+                                                    clay = entry_new_clay.get(),
+                                                    p =  entry_new_p.get(),
+                                                    k = entry_new_k.get(),
+                                                    ctc = entry_new_ctc.get(),
+                                                    v = entry_new_v.get(),
+                                                    ca = entry_new_ca.get(),
+                                                    mg = entry_new_mg.get()))
+insert_button.grid(row=8, column=0, columnspan=2, pady=10)
 
 select_frame("Menu")
 root.mainloop()
--- Insert_soil_data.py
+++ Insert_soil_data.py
@@ -3,5 +3,5 @@ import pandas as pd
 def insert_data(plot_id,clay,p,k,ctc,v,ca,mg):
     csv = pd.read_csv("Soil_Analysis.csv")
     csv.loc[len(csv)] = [plot_id,clay,p,k,ctc,v,ca,mg]
-    csv.to_csv("Soil_Analysis.csv")
+    csv.to_csv("Soil_Analysis.csv",index = False)
     return print(csv)
